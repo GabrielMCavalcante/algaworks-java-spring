494e4d0c185509753f7c9ee91ac542fc56269937
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-19 15:07:59 -0400

    Created BusinessException class

---
+++ src/main/java/com/algaworks/osworks/domain/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.algaworks.osworks.domain.exception;
+
+public class BusinessException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public BusinessException(String message) {
+		super(message);
+	}
+}
