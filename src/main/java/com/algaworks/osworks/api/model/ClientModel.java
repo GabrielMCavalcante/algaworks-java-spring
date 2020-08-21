82af308c40a06edbf9053b15f99748b923d13dc7
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-20 21:24:59 -0400

    Created ClientModel

---
+++ src/main/java/com/algaworks/osworks/api/model/ClientModel.java
@@ -0,0 +1,25 @@
+package com.algaworks.osworks.api.model;
+
+public class ClientModel {
+	
+	private Long id;
+	private String name;
+	
+	// Getters
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	// Setters
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
