5af708c58082722eae9b51d394affc63849048a2
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-08-20 21:26:18 -0400

    Created EntityNotFoundException

---
+++ src/main/java/com/algaworks/osworks/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,14 @@
+package com.algaworks.osworks.domain.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String description) {
+		super(description);
+		
+	}
+	
+	
+	
+}
